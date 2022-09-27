9ec6b0e2ed23b736d30b8afac9262eb4d93c9a1f
ShenSinclair <sinclair.she@gmail.com>
2022-09-27 22:42:16 +0000

    Comments added to hello_world

--- hello_world.py
+++ hello_world.py
@@ -1,1 +1,10 @@
-print("Hello World")
+#!/usr/bin/env python3.7
+
+#This is a full line comment
+
+print("Hello, World") #This is a trailing comment
+
+"""
+This is not a block comment
+This is a multiline string.
+"""
