3338c349c4cac62e777e1d592be2705e517c55f1
ShenSinclair <sinclair.she@gmail.com>
2022-09-28 20:48:56 +0000

    Created bio questionnaire

---
+++ bio.py
@@ -0,0 +1,10 @@
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
+age = int(input("How old are you today? "))
+food = input("What is your favorite food? ")
+hobby = input("What's your favorite hobby? ")
+
+print(name, end=" ")
+print("is " + str(age) + " years old", end=" ")
+print("and loves the color " + color + ".", end= " ")
+print(name + " also likes to " + hobby + " and favorite food is " + food + ".", end=" " )
