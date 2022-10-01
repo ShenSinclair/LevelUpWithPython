15ab52ff5751ca930cb865f5e6f11d4ab6882056
ShenSinclair <sinclair.she@gmail.com>
2022-10-01 22:46:29 +0000

    Created AWS services listing project.

---
+++ AWS_services_list.py
@@ -0,0 +1,27 @@
+#Create a list of AWS services
+#Project Requirements:
+
+#1. The list should be empty initially.
+#2. Populate the list using append or insert.
+#3. Print the list and the length of the list.
+#4. Remove two specific services from the list by name or by index.
+#5. Print the new list and the new length of the list.
+
+List = []
+
+List.append('S3') 
+List.append('DynamoDB')
+List.append('EC2') 
+List.append('Lambda')
+List.append('CodePipeline')
+
+print(List)
+
+List_length = str(len(List))
+print(List_length)
+
+del List[3:5]
+print(List)
+
+List_length = str(len(List))
+print(List_length)
