9079745aff312b8fd2c9dddc82fd66ee1680719f
ShenSinclair <sinclair.she@gmail.com>
2022-10-11 00:18:41 +0000

    added line of code that prints the number of S3 buckets

--- AWS_PythonCoursework/S3_search.py
+++ AWS_PythonCoursework/S3_search.py
@@ -1,6 +1,10 @@
+#This script allows you to search and print the number of S3 buckets that are available 
 import boto3
+
 resource=boto3.resource 
 resource=boto3.resource("s3")
 list(resource.buckets.all())
 
 print(list(resource.buckets.all()))
+
+print(len(list(resource.buckets.all())))
