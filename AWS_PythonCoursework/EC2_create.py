24be654e4950c7cf3e30988140ada6f2eb561797
ShenSinclair <sinclair.she@gmail.com>
2022-10-13 21:27:24 +0000

    Python script to create/launch EC2 instances.

--- AWS_PythonCoursework/EC2_create.py
+++ AWS_PythonCoursework/EC2_create.py
@@ -7,7 +7,7 @@ instances = ec2.create_instances(
         MinCount=1,
         MaxCount=1,
         InstanceType="t2.micro",
-        KeyName="ApacheWebServerKey",
+        KeyName="Key_Pair_Name",
         SubnetId="subnet-0e8027bdf19b9e9e2")
-        
+#to launch multiple instances change Min & Max Count to a number > 1. 
 print(instances)
