1f02d99fbf5d4ac36900850c325886e20b254ed5
ShenSinclair <sinclair.she@gmail.com>
2022-10-16 19:28:39 +0000

    script to query an item in a DynamoDB Table based on more than 1 specific attribute imformation.

---
+++ AWS_WorkshopLabs/MoviesQuery02.py
@@ -0,0 +1,28 @@
+#https://catalog.us-east-1.prod.workshops.aws/workshops/3d705026-9edc-40e8-b353-bdabb116c89c/en-US/persisting-data/dynamodb/step-4#query-all-movies-released-in-a-year-with-certain-titles
+
+import boto3
+import json
+import decimal
+from boto3.dynamodb.conditions import Key, Attr
+
+# Helper class to convert a DynamoDB item to JSON.
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, decimal.Decimal):
+            return str(o)
+        return super(DecimalEncoder, self).default(o)
+
+dynamodb = boto3.resource('dynamodb', region_name='us-east-1')
+
+table = dynamodb.Table('Movies')
+
+print("Movies from 1992 - titles A-L, with genres and lead actor")
+
+response = table.query(
+    ProjectionExpression="#yr, title, info.genres, info.actors[0]",
+    ExpressionAttributeNames={ "#yr": "year" }, # Expression Attribute Names for Projection Expression only.
+    KeyConditionExpression=Key('year').eq(1992) & Key('title').between('A', 'L')
+)
+
+for i in response[u'Items']:
+    print(json.dumps(i, cls=DecimalEncoder))
